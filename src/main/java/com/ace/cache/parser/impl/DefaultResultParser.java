b13ba282b282887e240579e731f69058f0ab9205
bill_ho <bill_ho@foxmail.com>
2019-12-08 11:06:44 +0800

    fix 多曾泛型引起的bug

--- src/main/java/com/ace/cache/parser/impl/DefaultResultParser.java
+++ src/main/java/com/ace/cache/parser/impl/DefaultResultParser.java
@@ -24,7 +24,9 @@ public class DefaultResultParser implements ICacheResultParser {
             ParameterizedType parameterizedType = (ParameterizedType) type;
             Type rawType = parameterizedType.getRawType();
             if (((Class) rawType).isAssignableFrom(List.class)) {
-                result = JSON.parseArray(value, (Class) parameterizedType.getActualTypeArguments()[0]);
+//                result = JSON.parseArray(value, (Class) parameterizedType.getActualTypeArguments()[0]);
+                //fix 多层泛型引起的bug
+                result = JSON.parseArray(value,parameterizedType.getActualTypeArguments());
             }
         } else if (origins == null) {
             result = JSON.parseObject(value, (Class) type);
